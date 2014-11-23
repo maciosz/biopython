71b20e409ef7eae245025a8c94c93e918a5e7fe5
maciosz <a.macioszek@students.mimuw.edu.pl>
2014-11-23 23:28:12 +0100

    test.py update to include changes  (parser.py splitted into Profile.py and ProfileIO.py;  reading from .sgr file)

--- Bio/Profile/test.py
+++ Bio/Profile/test.py
@@ -1,7 +1,16 @@
-import parser
+import Profile
 
-a = parser.Profile("sample_profile.wig", "wig")
+a = Profile.Profile("sample_profile.sgr","sgr")
+b = Profile.Profile("sample_profile.wig", "wig")
+print "nazwy i rozdzielczosci wczytanych profili sample_profile*:"
 print a.name, a.description, a.resolution
-#print a.values
+print b.name, b.description, b.resolution
+print "zapisuje sample_profile.sgr na proba.sgr i .wig, sample_profile.wig na proba1.sgr, .wig"
 a.writeProfile("proba.sgr","sgr")
 a.writeProfile("proba.wig","wig")
+b.writeProfile("proba1.sgr","sgr")
+b.writeProfile("proba1.wig","wig")
+print "wczytuje nieistniejacy plik"
+c = Profile.Profile("ala ma kota", "wig")
+print "podaje zly format"
+c = Profile.Profile("sample_profile.sgr","ala ma kota")
